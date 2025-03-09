5c054f6382c7d6063d6535c665665f5d0498035d
jjtorborg <john.torborg@gmail.com>
2025-03-08 22:29:07 -0600

    Add bootstrapped next.js hello world app

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+
+export default function Home() {
+  return (
+    <div
+      style={{ 
+        display: 'flex',
+        justifyContent: 'center' ,
+        alignItems:'center',
+        height: '100vh'
+      }}>
+      <div>
+        Hello world!
+      </div>
+    </div>
+  );
+}
