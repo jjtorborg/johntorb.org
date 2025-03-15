941f6cca0b12a2e7bf09b14cf7b39f305284b37d
jjtorborg <john.torborg@gmail.com>
2025-03-15 18:02:33 -0500

    Show initials in header on scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,27 @@
-import { headerFont } from "../app/fonts";
-import { Tabs } from "./Tabs";
+'use client'
+
+import { useState, useEffect } from 'react';
+import { headerFont } from '../app/fonts';
+import { Tabs } from './Tabs';
 
 export default function Header() {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 0);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    });
+
     return (
         <header className={`header ${headerFont.className}`}>
             <h1 className='header__title'>
-                JOHN TORBORG
+                {isScrolled ? 'JT' : 'JOHN TORBORG'}
             </h1>
             <Tabs tabs={
                 [
