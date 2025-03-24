1ece541fd10568a88cf630459ce8089908194be2
jjtorborg <john.torborg@gmail.com>
2025-03-23 22:08:22 -0500

    Tweak timeline and mobile intro text

--- src/components/page/Intro.tsx
+++ src/components/page/Intro.tsx
@@ -5,7 +5,7 @@ import { Boxes } from '../effects/Boxes';
 import { TypewriterEffectSmooth } from '../effects/Typewriter';
 
 export default function Intro() {
-    const nameClass = `${cn('md:text-8xl text-xl relative z-2 font-bold')}`;
+    const nameClass = `${cn('md:text-8xl text-4xl relative z-2 font-bold')}`;
     const name = [
         {
             text: 'John',
@@ -17,8 +17,8 @@ export default function Intro() {
         }
     ];
 
-    const titleClass = `${cn('md:text-4xl text-xl relative z-2')}`;
-    const titleClassBlue = `${cn('md:text-4xl text-xl relative z-2 text-blue-500')}`;
+    const titleClass = `${cn('md:text-4xl text-base relative z-2')}`;
+    const titleClassBlue = `${cn('md:text-4xl text-base relative z-2 text-blue-500')}`;
     const title = [
         {
             text: 'Full',
@@ -36,7 +36,7 @@ export default function Intro() {
 
     return (
         <div className='flex items-center justify-center w-screen'>
-            <div className='h-200 w-400 relative overflow-hidden bg-black flex flex-col items-center justify-center rounded-lg'>
+            <div className='md:h-200 md:w-400 h-175 w-200 relative overflow-hidden bg-black flex flex-col items-center justify-center rounded-lg'>
                 <div className='absolute inset-0 w-full h-full bg-black z-2 [mask-image:radial-gradient(var(--radial-gradient-stops))] pointer-events-none'
                     style={{ '--radial-gradient-stops': 'transparent 0%, black 80%' } as React.CSSProperties}
                 />
